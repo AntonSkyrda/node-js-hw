ebc7d5fde0585cdd764a62b1aa1ab3d37b79d2d0
Anton Skyrda <mr.skyrda@gmail.com>
2025-11-23 04:36:23 +0200

    done hw-7

---
+++ frontend/src/models/IUser.ts
@@ -0,0 +1,14 @@
+export interface IUser {
+	isVarified: boolean;
+	createdAt: string;
+	role: string;
+	isDeleted: boolean;
+	surname: string;
+	name: string;
+	_id: string;
+	isActive: boolean;
+	email: string;
+	age: number;
+	updatedAt: string;
+}
+
